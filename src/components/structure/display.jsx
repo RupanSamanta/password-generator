cc40163bc4568e352593dcbc178650d7f34be76e
Rupan Samanta <rvsjan32@gmail.com>
2026-05-31 19:09:12 +0530

    Add tooltip component with provider, trigger, and content for enhanced UI

--- src/components/structure/display.jsx
+++ src/components/structure/display.jsx
@@ -1,13 +1,33 @@
 import { Input } from "../ui/input"
 import { Button } from "../ui/button"
-import { CopyIcon } from "lucide-react"
+import { CopyIcon, Check, AlertCircle } from "lucide-react"
+import { useState } from "react"
 
 function Display({ password }) {
+    const [icon, setIcon] = useState(<CopyIcon />)
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(password)
+            .then(() => {
+                setIcon(<Check />);
+            })
+            .catch((err) => {
+                setIcon(<AlertCircle />);
+            }).finally(()=> {
+                setTimeout(()=> {
+                    setIcon(<CopyIcon />)
+                }, 1500);
+            })
+    }
     return (
         <>
             <Input type="text" value={password} id="password" placeholder="Your Password Here" disabled></Input>
-            <Button variant="outline" size="icon" aria-label="Copy" className="cursor-pointer">
-                <CopyIcon></CopyIcon>
+            <Button variant="outline" 
+                    size="icon" 
+                    aria-label="Copy" 
+                    className="cursor-pointer" 
+                    onClick={copyToClipboard}
+                    disabled={password.length === 0}>
+                {icon}
             </Button>
         </>
     )
